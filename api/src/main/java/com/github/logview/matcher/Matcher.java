cccc8c73ca7b92f74b10ea9089ac48cd65c7e3a8
Dennis Rieks <dennisrieks@googlemail.com>
2012-04-16 21:44:32 +0200

    new multi matcher

--- api/src/main/java/com/github/logview/matcher/Matcher.java
+++ api/src/main/java/com/github/logview/matcher/Matcher.java
@@ -1,6 +1,5 @@
 package com.github.logview.matcher;
 
-
 public interface Matcher {
 	Match match(String line);
 }
---
+++ api/src/main/java/com/github/logview/matcher/MultiMatcher.java
@@ -0,0 +1,42 @@
+package com.github.logview.matcher;
+
+import java.util.List;
+import java.util.Properties;
+
+import com.github.logview.value.api.ValueFactory;
+import com.google.common.collect.Lists;
+
+public class MultiMatcher implements Matcher {
+	private final ValueFactory factory;
+	private final List<Matcher> matchers = Lists.newLinkedList();
+
+	public MultiMatcher(ValueFactory factory, String key, Properties props) {
+		this.factory = factory;
+		for(int i = 0;; i++) {
+			String match = props.getProperty(key + "." + i);
+			if(match == null) {
+				break;
+			}
+			add(match);
+		}
+	}
+
+	@Override
+	public Match match(String line) {
+		synchronized(matchers) {
+			for(Matcher matcher : matchers) {
+				Match ret = matcher.match(line);
+				if(ret != null) {
+					return ret;
+				}
+			}
+		}
+		return null;
+	}
+
+	public boolean add(String match) {
+		synchronized(matchers) {
+			return matchers.add(new ValueMatcher(factory, match));
+		}
+	}
+}
